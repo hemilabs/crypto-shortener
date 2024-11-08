971dccfe4c55a3df5664775eb87ebb7baba50ba4
Gonzalo D'Elia <gonzalo@bloq.com>
2024-11-08 14:46:22 -0300

    Use native vitest coverage

--- knip.config.js
+++ knip.config.js
@@ -2,7 +2,11 @@
  * @type {import('knip').KnipConfig}
  */
 const config = {
-  ignoreDependencies: ["eslint-config-prettier", "lint-staged"],
+  ignoreDependencies: [
+    "@vitest/coverage-v8",
+    "eslint-config-prettier",
+    "lint-staged",
+  ],
 };
 
 export default config;
